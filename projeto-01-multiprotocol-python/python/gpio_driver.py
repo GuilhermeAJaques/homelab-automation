c8d4bda96a68909603b1fb359fc2569eb55547bf
GuilhermeAJaques <jaquesbr@outlook.com>
2026-07-11 21:29:39 -0300

    Project 01: Update GPIO read and write method

--- projeto-01-multiprotocol-python/python/gpio_driver.py
+++ projeto-01-multiprotocol-python/python/gpio_driver.py
@@ -1,40 +1,37 @@
 import gpiod
+from gpiod.line import Direction, Value
 
 class GPIO_driver:
     # Just declare to match with other drivers
     def __init__(self, chip):
         try:
-            self.chip = gpiod.Chip(chip)
+            self.chip = chip
         except Exception as e:
             print("Error connectiong to gpiochip0: {}".format(e)) 
 
     def read_variable(self, gpio_number):
         try:
-            line = self.chip.get_line(gpio_number)
-            line.request(consumer="reader", type=gpiod.LINE_REQ_DIR_IN)
-            value = line.get_value()
-            line.release()
-            return value
+            with gpiod.request_lines(self.chip, 
+                                     consumer="reader",
+                                     config={gpio_number: gpiod.LineSettings(direction=Direction.INPUT)}) as request:
+                value = request.get_value(gpio_number)
+                return value
         except Exception as e:
             print("Error to read {}: {}".format(gpio_number, e)) 
-            try:
-                line.release()
-            except:
-                pass
             return None
 
     def write_variable(self, gpio_number, value):
         try:
-            line = self.chip.get_line(gpio_number)
-            line.request(consumer="writer", type=gpiod.LINE_REQ_DIR_OUT)
-            line.set_value(value)
-            line.release()
+            with gpiod.request_lines(self.chip,
+                                     consumer="writer", 
+                                     config={gpio_number: gpiod.LineSettings(direction=Direction.OUTPUT)}) as request:
+                if (bool(value)):
+                    request.set_value(gpio_number, Value.ACTIVE)
+                else:
+                    request.set_value(gpio_number, Value.INACTIVE)
+
         except Exception as e:
             print("Error to write {}: {}".format(gpio_number, e)) 
-            try:
-                line.release()
-            except:
-                pass
 
     # Just declare to match with other drivers, otherwise will throw error
     def connect(self):
@@ -42,8 +39,4 @@ class GPIO_driver:
 
     # Just declare to match with other drivers, otherwise will throw error
     def disconnect(self):
-        try:
-            self.chip.close()
-        except Exception as e:
-            print("Error close connection to GPIO: {}".format(e)) 
         print("GPIO driver disconnected")
